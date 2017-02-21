259c379b33491def738d0f8d1ae8afe75910ed68
chenshaojian <879937342@qq.com>
2017-02-21 13:48:33 +0800

    first commit

---
+++ inno_ips1/urls.py
@@ -0,0 +1,28 @@
+"""inno_ips URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^training_result/$', 'api.views.training_result'),
+    url(r'^home/$', 'api.views.home'),
+    url(r'^ips/$', 'api.views.ips_data'),
+    url(r'^black/$', 'api.views.blacklist'),
+    url(r'^allstore/$', 'api.views.allstore_ips'),
+    url(r'^insert/$', 'api.views.insert_data'),
+
+]
